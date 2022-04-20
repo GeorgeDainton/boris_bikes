1c50aa581185dc655cdebfbf1ebf1542f99b1232
Luke Storey <lukemstorey@gmail.com>
2022-04-20 16:31:22 +0100

    Add test for seeing bikes in docking station

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,5 +1,14 @@
 class DockingStation
+  attr_accessor :bikes
+
+  def initialize(bikes)
+    @bikes = [bikes]
+  end
+
   def release_bike
     Bike.new
   end 
+
+  def dock(bike)
+  end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,11 +1,19 @@
 require 'docking_station'
 
 describe DockingStation do
-  let(:docking_station) { DockingStation.new }
+  let(:bike) { Bike.new }
+  let(:docking_station) { DockingStation.new(bike) }
+  
   it "should release bike" do
     expect(docking_station.release_bike)
   end 
   it "should return an instance of Bike" do
     expect(docking_station.release_bike).to be_a Bike
+  end
+  it "should dock bike" do
+    expect(docking_station.dock(bike))
+  end
+  it "should display bikes" do
+    expect(docking_station.bikes).to eq [bike]
   end
 end
