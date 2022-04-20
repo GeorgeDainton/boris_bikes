c9ca1decd588dee2202f63df9a07acc8a54247e5
Luke Storey <lukemstorey@gmail.com>
2022-04-20 16:31:06 +0100

    Add test for working bike

--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -5,4 +5,9 @@ describe Bike do
     bike = Bike.new
     expect(bike.working?)
   end 
+
+  it "should return a working Bike" do
+    bike = Bike.new
+    expect(bike.working?).to be_truthy
+  end
 end  
